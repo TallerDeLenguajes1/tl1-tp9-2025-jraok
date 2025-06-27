c033dce3b1c5eda7ebc73bbfce0de9785b6b464f
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-27 01:23:59 -0300

    creacion de la carpeta MP3 con canciones por defecto y el lector de archivos MP3

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+Console.Clear();
+string path = "MP3/";
+DirectoryInfo carpeta = new DirectoryInfo(path);
+
+if (carpeta.Exists)
+{
+    FileInfo[] canciones = carpeta.GetFiles("*.mp3");
+    if (canciones.Length > 0)
+    {
+        Console.WriteLine("\n\t\t---LISTA DE CANCIONES---");
+        foreach (FileInfo cancion in canciones)
+        {
+            Console.WriteLine($"\t|--> {cancion.Name}");
+        }
+    }else
+    {
+        Console.WriteLine("\n\t\t---Sin Canciones---");
+    }
+}else{
+    Console.WriteLine("\n\t\t---LA CARPETA NO EXISTE----");
+}
