11a9fd22d77042b3e8047c78ac122be193b1f3dd
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-26 21:26:53 -0300

    cree las variables para manejar la iteraccion con el usuario

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.IO;
+
+string path;
+bool continuar = true;
+do
+{
+    Console.Clear();
+    Console.WriteLine(new string('-', 100)); // separador visual
+    Console.WriteLine("\n\t\tTALLER DE LENGUAJES I");
+    Console.Write("\n\tIngrese el direcorio a examinar (ENTER PARA CANCELAR): ");
+    path = Console.ReadLine();
+    if (string.IsNullOrEmpty(path)) break;
+    DirectoryInfo directorio = new DirectoryInfo(path);
+
+
+    do{
+        Console.Write("\n\t\tDesea continuar? (ESC = NO | ENTER = SÍ): ");
+        ConsoleKey tecla = Console.ReadKey(true).Key;
+        if(tecla == ConsoleKey.Escape || tecla == ConsoleKey.Enter){    /* solo entra si se apreto esc o enter */
+            continuar = (tecla == ConsoleKey.Enter) ? true : false; /* le asigno un valor a continuar segun la tecla que se apreto */
+            break;
+        }
+        Console.WriteLine("\n\t\t---OPCIÓN NO VALIDA, REINGRESE---");
+    }while(true); /* no es necesario cambiar la condicion pues cuando se cumplae el if se rompe el bucle */
+} while (continuar);
