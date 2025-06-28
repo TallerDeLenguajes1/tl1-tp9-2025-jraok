3662f87c3091bcf151e1171784c6298143e02f89
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-28 00:01:23 -0300

    Menu para seleccionar una cancion de la lista

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Internal;
 
 Console.Clear();
 string path = "MP3/";
@@ -10,11 +11,23 @@ if (carpeta.Exists)
     FileInfo[] canciones = carpeta.GetFiles("*.mp3");
     if (canciones.Length > 0)
     {
-        Console.WriteLine("\n\t\t---LISTA DE CANCIONES---");
-        foreach (FileInfo cancion in canciones)
+        int i = 0;
+        bool salir = true;
+        do
         {
-            Console.WriteLine($"\t|--> {cancion.Name}");
-        }
+            Console.WriteLine("\n\t\t---LISTA DE CANCIONES---");
+            foreach (FileInfo cancion in canciones)
+            {   
+                Console.WriteLine($"\t|--> {++i + "_" + cancion.Name}");
+            }
+            Conosle.Write("\n\t\t---Seleccione una canción (ÍNDICE): ");
+            string indice = Console.read();
+            if (!int.TryParse(indice, out i) || i < 1 || i > canciones.Length){
+                Console.WriteLine("\n\t\t---ÍNDICE INVÁLIDO---");
+            }else{
+                salir = false;
+            }
+        } while (salir);
     }else
     {
         Console.WriteLine("\n\t\t---Sin Canciones---");
