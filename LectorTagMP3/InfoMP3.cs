932628f5f0994b660fcc5c1dfb9e913fc68318d5
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-28 01:46:08 -0300

    nuevo metodo para crear una instancia de MP3 con la informacion

--- LectorTagMP3/InfoMP3.cs
+++ LectorTagMP3/InfoMP3.cs
@@ -1,3 +1,8 @@
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
 namespace EspacioMP3;
 // clase para la informacion de los archivos MP3
 public class InfoMP3
@@ -8,9 +13,30 @@ public class InfoMP3
     private string album;
     private int anio;
     private string comentario;
-    private string genero;
+    private byte genero;
 
+    public static InfoMP3 LeerBytes(byte[] arreglo){
+        if (arreglo == null || arreglo.Length != 128)
+        {
+            throw new ArgumentException("Datos insuficientes para procesar");
+        }
 
+        string tag = Encoding.ASCII.GetString(arreglo, 0, 3);
+        if (tag != "TAG"){
+            throw new InvalidDataException("El formato incorrecto, falta ID3v1");
+        }
+        string titulo = Encoding.ASCII.GetString(arreglo, 3, 30);
+        string artista = Encoding.ASCII.GetString(arreglo, 33, 30);
+        string album = Encoding.ASCII.GetString(arreglo, 63, 30);
+        string anioArreglo = Encoding.ASCII.GetString(arreglo,93, 4 );
+        string comentario = Encoding.ASCII.GetString(arreglo, 97, 30);
+        byte genero = arreglo[127];
+
+        int anio = 0;
+        int.TryParse(anioArreglo, out anio);
+
+        return new InfoMP3(titulo, artista, album, anio, comentario, genero);
+    }
 
 
     // propiedades publicas para la lectura de los campos
@@ -19,6 +45,6 @@ public class InfoMP3
     public string Album => album;
     public int Anio => anio;
     public string Comentario => comentario;
-    public string Genero => genero;
+    public byte Genero => genero;
 
 }
