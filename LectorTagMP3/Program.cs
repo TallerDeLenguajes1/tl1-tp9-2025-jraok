97a6b45f91c3b4c034bb264cfa19225fec17187d
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-28 02:37:37 -0300

    correcion de warning sobre linea 14, lógica completa del try catch para la lectura del archivo y la impresion de la información

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using EspacioMP3;
 
 // ruta por defecto con los archivos MP3
 string path = "MP3/";
@@ -10,9 +11,11 @@ if (carpeta.Exists) /* verifico la existencia de la ruta */
     FileInfo[] canciones = carpeta.GetFiles("*.mp3");   /* arreglo con solo los archivos MP3 de la ruta */
     if (canciones.Length > 0)   /* si encuentra archivos en la ruta */
     {
+        FileInfo? cancion = null;
         /* bucle para mostrar las canciones y elegir una de la lista */
         do
         {
+            System.Threading.Thread.Sleep(1200);    /* pausa para mostrar el mensaje */
             Console.Clear();    /* limpieza de la terminal */
             Console.Write(new string('-', 80)); /* separador visual */
             Console.WriteLine("\n\t\t---TALLER DE LENGUAJES I---");
@@ -32,11 +35,33 @@ if (carpeta.Exists) /* verifico la existencia de la ruta */
                 System.Threading.Thread.Sleep(1200);    /* pausa para mostrar el mensaje */
                 continue;
             }else{
+                cancion = canciones[posicion-1];    /* asignacion de la cancion elegida */
                 break;  /* salida del bucle */
             }
         } while (true);
-    }else
-    {
+        
+        try
+        {
+            byte[] arreglo = new byte[128];    /* arreglo para guardar los datos de la cancion */
+            using FileStream MiCancion = new FileStream(cancion.FullName, FileMode.Open);    /* instancia para leer el archivo */
+            MiCancion.Seek(-128, SeekOrigin.End); /* posicionamiento al final del archivo */
+            MiCancion.ReadExactly(arreglo, 0, 128); /* lectura de los datos de la cancion */
+
+            InfoMP3 informacion = InfoMP3.LeerBytes(arreglo);   /* instancia con la informacion de la cancion */
+            // impresion de la informacion de la cancion
+            Console.WriteLine($"\n\t\t---INFORMACION DE LA CANCIÓN---");
+            Console.WriteLine($"\t TÍTULO: {informacion.Titulo}");
+            Console.WriteLine($"\t ARTISTA: {informacion.Artista}");
+            Console.WriteLine($"\t ÁLBUM: {informacion.Album}");
+            Console.WriteLine($"\t AÑO: {informacion.Anio}");
+            Console.WriteLine($"\t COMENTARIO: {informacion.Comentario}");
+            Console.WriteLine($"\t GÉNERO: {informacion.Genero}");
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine($"\n\t\tERROR: {error.Message}"); /* mensaje de la exepcion dentro del metodo LeerBytes */
+        }
+    }else{
         Console.WriteLine("\n\t\t---Sin Canciones---");
     }
 }else{
