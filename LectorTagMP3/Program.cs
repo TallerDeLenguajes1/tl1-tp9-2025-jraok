1f9d3b2822899db6f07e3404c7bc7b0d68098010
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-28 00:11:31 -0300

    correcciones de sintaxis, mejora en los mensajes y lógica

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
-using Internal;
 
-Console.Clear();
 string path = "MP3/";
 DirectoryInfo carpeta = new DirectoryInfo(path);
 
@@ -11,19 +9,24 @@ if (carpeta.Exists)
     FileInfo[] canciones = carpeta.GetFiles("*.mp3");
     if (canciones.Length > 0)
     {
-        int i = 0;
+        int i;
         bool salir = true;
         do
         {
+            Console.Clear();
+            Console.Write(new string('-', 80));
             Console.WriteLine("\n\t\t---LISTA DE CANCIONES---");
+            i = 0;
             foreach (FileInfo cancion in canciones)
             {   
-                Console.WriteLine($"\t|--> {++i + "_" + cancion.Name}");
+                Console.WriteLine($"\t|--> {++i + "_ " + cancion.Name}");
             }
-            Conosle.Write("\n\t\t---Seleccione una canción (ÍNDICE): ");
-            string indice = Console.read();
+            Console.Write("\n\t\tSeleccione una canción (ÍNDICE): ");
+            string indice = Console.ReadLine();
             if (!int.TryParse(indice, out i) || i < 1 || i > canciones.Length){
                 Console.WriteLine("\n\t\t---ÍNDICE INVÁLIDO---");
+                System.Threading.Thread.Sleep(1200);
+                continue;
             }else{
                 salir = false;
             }
