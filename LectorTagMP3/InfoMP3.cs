3946ff05abce9dfbb3f3953a052ac575227fe41d
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-28 01:25:23 -0300

    nueva clase InfoMP3, con los campos y propiedades para guardar y leer la información de un MP3

---
+++ LectorTagMP3/InfoMP3.cs
@@ -0,0 +1,24 @@
+namespace EspacioMP3;
+// clase para la informacion de los archivos MP3
+public class InfoMP3
+{
+    // campos privados con la información de la clase
+    private string titulo;    
+    private string artista;
+    private string album;
+    private int anio;
+    private string comentario;
+    private string genero;
+
+
+
+
+    // propiedades publicas para la lectura de los campos
+    public string Titulo => titulo;
+    public string Artista => artista;
+    public string Album => album;
+    public int Anio => anio;
+    public string Comentario => comentario;
+    public string Genero => genero;
+
+}
